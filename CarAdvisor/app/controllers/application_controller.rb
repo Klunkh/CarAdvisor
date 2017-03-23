de9793da0710636ae61bb09986cb85bbe7ca54f5
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2017-03-23 15:17:23 +0100

    commit in presenza di errori?

--- CarAdvisor/app/controllers/application_controller.rb
+++ CarAdvisor/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def hello
-    render html: "CarAdvisor"
+    render html: "Proviamo a iniziare CarAdvisor"
   end
 end
