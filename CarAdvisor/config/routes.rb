a2a4369c0526c5b88d233d03e4b5969deab5ac21
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2017-03-23 15:04:42 +0100

    prova

--- CarAdvisor/config/routes.rb
+++ CarAdvisor/config/routes.rb
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
  Rails.application.routes.draw do
   root 'application#hello'
 end
+=======
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+>>>>>>> 2ed5e5fa8d4af5b948a4f931a3a7f92ffbe3e64f
