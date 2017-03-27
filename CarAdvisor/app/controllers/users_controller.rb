404f07a767ad9b853ccc85f570e5214d23435e48
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2017-03-27 18:22:07 +0200

    Fix Registrazione

--- CarAdvisor/app/controllers/users_controller.rb
+++ CarAdvisor/app/controllers/users_controller.rb
@@ -28,14 +28,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
+      
       if @user.save
 	log_in @user
 	flash[:success] = "Benvenuto in CarAdvisor"
-        redirect_to "http//localhost:3000/"
+        #redirect_to "http//localhost:3000/"
+        format.html { redirect_to @user, notice: 'Account creato correttamente.' }
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    
     end
   end
 
@@ -43,12 +47,15 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
+      
+	
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+      
       end
     end
   end
