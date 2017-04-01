d25c931854cda1aba68278bb11659e507ba2c756
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2017-04-01 17:05:49 +0200

    tanta roba

--- CarAdvisor/app/helpers/users_helper.rb
+++ CarAdvisor/app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+    image_tag(gravatar_url, alt: user.nome, class: "gravatar")
+  end
 end
--- CarAdvisor/db/schema.rb
+++ CarAdvisor/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401091829) do
+ActiveRecord::Schema.define(version: 20170401135514) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nome"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170401091829) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
